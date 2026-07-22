2347855ed464502d364122f8ea97554d3e92c844
saileshchikkam <sailesh.ch118@gmail.com>
2026-07-22 16:20:34 +0530

    Modules, Packages and standard library files are  updated

---
+++ Python/Modules and Packages/Package_Folder/mathematics.py
@@ -0,0 +1,15 @@
+def addition(a,b):
+    return a+b
+def subtraction(c,d):
+    if c>d:
+        return c-d
+    else:
+        return d-c
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    if b==0:
+        return "Enter valid value"
+    else:
+        return a/b
+    
---
+++ Python/Modules and Packages/Package_Folder/subpackage/sum.py
@@ -0,0 +1,2 @@
+def addition (a,b,c,d):
+    return a+b+c+d
