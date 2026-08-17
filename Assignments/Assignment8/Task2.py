35c3234e0b1489c35232e73445be03934b01ca49
saileshchikkam <sailesh.ch118@gmail.com>
2026-08-17 20:47:08 +0530

    updated

--- Assignments/Assignment8/Task2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-Product_price = st.number_input("Product Price: ")
-discount = st.slider("Discount Percentage: ",0,50)
-if st.button("Click here to calculate discounted price: "):
-    discount_price = Product_price - (Product_price * discount / 100)
-st.success(f"The discounted price is: ₹{discount_price:.2f}")
